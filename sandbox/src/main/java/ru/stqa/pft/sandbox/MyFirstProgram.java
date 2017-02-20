21b6871ea123b29ec934df112ef040c555bdfb21
Yulia <yulya.vasilevskaya@gmail.com>
2017-02-20 22:49:00 +0300

    Задание 2: расстояние между двумя точками + ИСПРАВЛЕНИЯ
    Исправлено в соответствии с комментарием:
    "Класс Point должен описывать одну точку.
    Метода расчета расстояний хорошо сделать, чтобы так вызвался:
    a.distance(b)"

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,12 +3,28 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
+    hello("world");
+    hello("Yulia");
+    hello("sun");
+    hello("spring");
 
-    Point p = new Point(1, 2, 3, 4);
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    System.out.println("Координаты точки x(" + p.x + ";" + p.y + ")");
-    System.out.println("Координаты точки y(" + p.x2 + ";" + p.y2 + ")");
-    System.out.println("Расстояние между точками " + "x и y" + " = " + p.distance());
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+
+    Point p1 = new Point(1, 2);
+    Point p2 = new Point(3, 4);
+
+    System.out.println("Координаты точки x(" + p1.x + ";" + p1.y + ")");
+    System.out.println("Координаты точки y(" + p2.x + ";" + p2.y + ")");
+    System.out.println("Расстояние между точками " + "x и y" + " = " + p1.distance(p2));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello," + somebody + "!:)");
   }
 }
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,25 +4,22 @@ package ru.stqa.pft.sandbox;
  * Created by Yulia on 2/17/2017.
  */
 
-public class Point {
+class Point {
+  int x, y;
 
-  public double x;
-  public double y;
-  public double x2;
-  public double y2;
-
-
-  public Point(double х, double y, double х2, double y2) {
+  Point(int х,int y) {
     this.x = х;
     this.y = y;
-    this.x2 = х2;
-    this.y2 = y2;
   }
 
-  public double distance() {
-    double dx = this.x2 - this.x;
-    double dy = this.y2 - this.y;
-    return Math.sqrt((dx * dx + dy * dy));
+  double distance(int x, int y) {
+    int dx = this.x - x;
+    int dy = this.y - y;
+    return Math.sqrt(dx * dx + dy * dy);
   }
 
+  double distance(Point p) {
+    return distance(p.x, p.y);
+  }
 }
+
