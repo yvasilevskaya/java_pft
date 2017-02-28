91ac78062f378ce28e80d31bd39319c33fa6f1f3
Yulia <yulya.vasilevskaya@gmail.com>
2017-02-28 21:52:39 +0300

    Group Modification Test без использования рекордера

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -28,14 +28,25 @@ public class GroupHelper extends HelperBase {
   }
 
   public void initGroupCreation() {
+
     click(By.name("new"));
   }
 
   public void deleteSelectedGroups() {
+
     click(By.name("delete"));
   }
 
   public void selectGroup() {
+
     click(By.name("selected[]"));
+  }
+
+  public void initGroupModification() {
+    click(By.name("edit"));
+  }
+
+  public void submitGroupModification() {
+    click(By.name("update"));
   }
 }
