6fa11bc8e1462569c7b02486fb0511403aec9cb5
Yulia <yulya.vasilevskaya@gmail.com>
2017-03-14 23:34:06 +0300

    Поиск максимального

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,40 +1,27 @@
 package ru.stqa.pft.addressbook.model;
 
 public class GroupData {
-  private final String id;
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
 
-  public GroupData(String id, String name, String header, String footer) {
+  public GroupData(String name, String header, String footer) {
+    this.id = 0;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  public GroupData(int id, String name, String header, String footer) {
     this.id = id;
     this.name = name;
     this.header = header;
     this.footer = footer;
-  }
-  public GroupData(String name, String header, String footer) {
-    this.id = name;
-    this.name = name;
-    this.header = header;
-    this.footer = footer;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    GroupData groupData = (GroupData) o;
-
-    if (id != null ? !id.equals(groupData.id) : groupData.id != null) return false;
-    return name != null ? name.equals(groupData.name) : groupData.name == null;
-  }
-
-  @Override
-  public int hashCode() {
-    int result = id != null ? id.hashCode() : 0;
-    result = 31 * result + (name != null ? name.hashCode() : 0);
-    return result;
   }
 
   @Override
@@ -45,7 +32,25 @@ public class GroupData {
             '}';
   }
 
-  public String getId() {
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    if (id != groupData.id) return false;
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
+  }
+
+  public int getId() {
     return id;
   }
 
@@ -60,4 +65,6 @@ public class GroupData {
   public String getFooter() {
     return footer;
   }
+
+
 }
