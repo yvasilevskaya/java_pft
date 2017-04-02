a51c74ea429ceb13ff9856cbcfadf6d1c092d9ea
Yulia <yulya.vasilevskaya@gmail.com>
2017-04-02 17:17:08 +0300

    Задание №15: Параметризовать тесты для создания групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,17 +1,59 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
+
+  @DataProvider
+  public Iterator<Object[]> validContactsFromXml() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+      xml +=line;
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+    return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
+
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json +=line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());//List<ContactData>.class
+    return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
 
   @BeforeMethod
   public void ensurePreconditions(){
@@ -21,15 +63,15 @@ public class ContactCreationTests extends TestBase {
     }
   }
 
-  @Test
-  public void testContactCreation() {
+  @Test(dataProvider = "validContactsFromJson")
+  public void testContactCreation(ContactData contact) {
     app.goTo().HomePage();
     Contacts before = app.contact().all();
-    File photo = new File("src/test/resources/images.jpg");
-    ContactData contact = new ContactData()
-            .withName("Yulia").withLastName("LastName").withGroup("[none]").withGroup("[none]").withAddress("Minsk")
-            .withHomePhone("123 123").withMobilePhone("+37529").withWorkPhone("11 22")
-            .withEmail("Test@gmail.com").withEmail2("Test2@gmail.com").withEmail3("Test3@gmail.com").withPhoto(photo);
+//    File photo = new File("src/test/resources/images.jpg");
+//          .withName("Yulia").withLastName("LastName").withGroup("[none]").withGroup("[none]").withAddress("Minsk")
+//            .withHomePhone("123 123").withMobilePhone("+37529").withWorkPhone("11 22")
+//            .withEmail("Test@gmail.com").withEmail2("Test2@gmail.com").withEmail3("Test3@gmail.com");
+//            .withPhoto(photo);
     app.contact().create(contact);
     app.goTo().HomePage();
     assertThat(app.contact().сount(), equalTo(before.size() + 1));
