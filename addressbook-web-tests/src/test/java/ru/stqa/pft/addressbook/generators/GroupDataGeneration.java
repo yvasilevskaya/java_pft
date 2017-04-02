c0923a1379f52bb41103dea7f11a92d39088feac
Yulia <yulya.vasilevskaya@gmail.com>
2017-04-02 16:03:12 +0300

    Формат Json

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGeneration.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGeneration.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -46,9 +48,28 @@ public class GroupDataGeneration {
       saveAsCsv(groups, new File(file));
     }else if (format.equals("xml")) {
       saveAsXml(groups, new File(file));
+    }else if (format.equals("json")) {
+      saveAsJson(groups, new File(file));
     }else {
       System.out.println("Unrecognized format "+ format);
     }
+  }
+
+  private List<GroupData> generateGroups(int count) {
+    List<GroupData> groups = new ArrayList<GroupData>();
+    for (int i = 0; i < count; i++) {
+      groups.add(new GroupData().withName(String.format("test %s", i))
+              .withHeader(String.format("header\n%s", i)).withFooter(String.format("footer\n%s", i)));
+    }
+    return groups;
+  }
+
+  private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+    Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+    String json = gson.toJson(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(json);
+    writer.close();
   }
 
   private void saveAsXml(List<GroupData> groups, File file) throws IOException {
@@ -69,13 +90,5 @@ public class GroupDataGeneration {
     writer.close();
   }
 
-  private List<GroupData> generateGroups(int count) {
-    List<GroupData> groups = new ArrayList<GroupData>();
-    for (int i = 0; i < count; i++) {
-      groups.add(new GroupData().withName(String.format("test %s", i))
-              .withHeader(String.format("header\n%s", i)).withFooter(String.format("footer\n%s", i)));
-    }
-    return groups;
-  }
 }
 
